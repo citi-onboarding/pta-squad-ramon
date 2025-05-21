5ea137a27b8341c9130c34b0954c883fa254b23a
karinalimaklo <karinalimaolvr@gmail.com>
2025-05-21 14:53:37 -0300

    feature/header-navigation: criação do componente header e importação de imagens

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,3 +1,6 @@
 import LogoCITi from "./Logo.png";
-
+import LogoCITipet from "./header/logotipo citi pet.png"
+import Heart from "./header/favorite.png"
+import LogotipoCITi from "./header/Logotipo CITi 1.png"
 export { LogoCITi };
+export { LogoCITipet, LogotipoCITi, Heart }
