a0a6e3bb180833b1174d0c07301838f8ebccb311
karinalimaklo <karinalimaolvr@gmail.com>
2025-05-22 19:50:35 -0300

    feature/header-navigation: necessary adjustments

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,6 +1,6 @@
 import LogoCITi from "./Logo.png";
-import LogoCITipet from "./header/logotipo citi pet.png"
-import Heart from "./header/favorite.png"
-import LogotipoCITi from "./header/Logotipo CITi 1.png"
+import LogoCITipet from "./header/logotipo citi pet.svg"
+import Heart from "./header/favorite.svg"
+import LogotipoCITi from "./header/Logotipo CITi 1.svg"
 export { LogoCITi };
 export { LogoCITipet, LogotipoCITi, Heart }
