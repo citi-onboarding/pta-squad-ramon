5bc9d79d3795d535d6609c29a8853af1b84b25a7
romero003 <153675281+romero003@users.noreply.github.com>
2025-05-22 12:45:59 -0300

    feat (button): adds full width prop to button

--- client/src/components/Buttons/index.tsx
+++ client/src/components/Buttons/index.tsx
@@ -2,20 +2,22 @@ import React from "react";
 
 interface BotaoAcaoProps {
   texto: string;
-  cor: string; // define a cor de fundo
-  icone?: React.ReactNode; // pode ser qualquer ícone (Lucide, SVG, etc)
+  cor: string; 
+  icone?: React.ReactNode; 
   onClick: (() => void) | undefined;
 };
 
 export function BotaoAcao({ texto, cor, icone, onClick }: BotaoAcaoProps) {
   return (
+    
     <button
       onClick={onClick}
-      className={`flex items-center justify-center gap-2 px-4 py-2 text-white rounded-md ${cor}`}
+      className={`flex items-center justify-center gap-2.5 px-8 py-3 text-white rounded-full w-full ${cor}`}
     >
       {icone && <span>{icone}</span>}
       <span>{texto}</span>
     </button>
+    
   );
 }
      
