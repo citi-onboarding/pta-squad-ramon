a803714aa206a8eb610929b1b211d33b1b1e789b
karinalimaklo <karinalimaolvr@gmail.com>
2025-05-23 10:00:26 -0300

    feat/card-consulta-pet: pet card creation

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,6 +1,4 @@
 import LogoCITi from "./Logo.png";
-import LogoCITipet from "./header/logotipo citi pet.png"
-import Heart from "./header/favorite.png"
-import LogotipoCITi from "./header/Logotipo CITi 1.png"
+import Cat from "./cardpet/cat 4.svg"
 export { LogoCITi };
-export { LogoCITipet, LogotipoCITi, Heart }
+export { Cat }
