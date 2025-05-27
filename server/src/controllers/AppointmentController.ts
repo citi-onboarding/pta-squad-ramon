e12e30ac46efda63080cd579e660c35edc58d894
romero003 <153675281+romero003@users.noreply.github.com>
2025-05-26 22:48:42 -0300

    feat(crud-consulta): creates files for appointment crud

---
+++ server/src/controllers/AppointmentController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import { Citi, Crud } from "../global";
+
+class AppointmentController implements Crud {
+  constructor(private readonly citi = new Citi("User")) {}
+  create = async (request: Request, response: Response) => {
+    const { type, doctorName, dateTime, description, patientId } = request.body;
+
+    const isAnyUndefined = this.citi.areValuesUndefined(
+      type, 
+      doctorName, 
+      dateTime, 
+      description, 
+      patientId
+    );
+    if (isAnyUndefined) return response.status(400).send();
+
+    const newApointment = {  type, doctorName, dateTime, description, patientId  };
+    const { httpStatus, message } = await this.citi.insertIntoDatabase(newApointment);
+
+    return response.status(httpStatus).send({ message });
+  };
+
+  get = async (request: Request, response: Response) => {
+    try{
+        const { httpStatus, values } = await this.citi.getAll();
+    } catch (error) {
+        return response.status(500).send({ message: "Internal server error" });
+    }
+}
+
+  delete = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const { httpStatus, messageFromDelete } = await this.citi.deleteValue(id);
+    return response.status(httpStatus).send({ messageFromDelete });
+  };
+
+  update = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const { type, doctorName, dateTime, description, patientId } = request.body;
+
+    const updatedValues = { type, doctorName, dateTime, description, patientId };
+
+    const { httpStatus, messageFromUpdate } = await this.citi.updateValue(
+      id,
+      updatedValues
+    );
+
+    return response.status(httpStatus).send({ messageFromUpdate });
+  };
+
+}
+export default new AppointmentController();
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import userController from "../controllers/UserController";
 import PatientController from '../controllers/PatientController';
+import AppointmentController from "src/controllers/AppointmentController";
 
 const routes = express.Router();
 
@@ -14,6 +15,12 @@ routes.get('/patients/:id', PatientController.findById);
 routes.post('/patients', PatientController.create); 
 routes.put('/patients/:id', PatientController.update); 
 routes.delete('/patients/:id', PatientController.delete);
+
+routes.get('/consultas', AppointmentController.get);
+routes.get('/consultas/:id', AppointmentController.findById);
+routes.post('/consultas', AppointmentController.create);
+routes.put('/consultas/:id', AppointmentController.update);
+routes.delete('/consultas/:id', AppointmentController.delete);
 
 export default routes;
 
