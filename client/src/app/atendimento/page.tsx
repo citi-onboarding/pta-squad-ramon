2090209b51842b6a8f4641c961eb86f06d1383cd
dudarmouras <dudarmouras@gmail.com>
2025-05-28 15:28:08 -0300

    feat/tela-atendimento:  working day girls dev updates

--- client/src/app/atendimento/page.tsx
+++ client/src/app/atendimento/page.tsx
@@ -1,28 +1,55 @@
 "use client";
-
+import { addDays, format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { DateRange } from "react-day-picker"
+import { cn } from "@/lib/utils"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 import * as React from "react";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { format } from "date-fns";
 import { Bot, Calendar as CalendarIcon } from "lucide-react";
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { BotaoAcao } from "@/components/Buttons";
 import Header from "@/components/Header";
 import CardConsultaPet from "@/components/CardConsultaPet";
 import { CirclePlus } from "lucide-react";
+import { useState } from "react";
+
+export function DatePickerWithRange({
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(2022, 0, 20),
+    to: addDays(new Date(2022, 0, 20), 20),
+  })
+}
 
 export default function TelaAtendimento() {
+  const [medicoSelecionado, setMedicoSelecionado] = useState(""); 
   return (
-    <div className="h-screen w-screen border border-blue-700">
+    <div className="h-screen">
       <Header />
-      <div className="flex flex-col pt-[48px] border border-purple-800 mx-[10%]">
+      <div className="flex flex-col pt-[48px] mx-[10%]">
         <div className="flex flex-col ">
           <h1 className="text-5xl text-black font-source-code font-bold flex">
             Atendimento
@@ -30,19 +57,85 @@ export default function TelaAtendimento() {
           <div className="flex flex-col pt-[32px]">
             <p className="font-normal text-2xl">Qual é o médico?</p>
             <div className="flex flex-row pt-[24px] space-x-6">
-              <Input
-                type="text"
-                className="border border-black rounded-lg w-[520px] h-[50px] font-normal text-[#D9D9D9]"
-                placeholder="Pesquise aqui..."
-              />
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                    <Input
+                      type="text"
+                      className="border border-black rounded-lg w-[520px] h-[50px] font-normal text-[#D9D9D9]"
+                      placeholder="Pesquise aqui..."
+                      value={medicoSelecionado}
+                      readOnly
+                    />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                      <DropdownMenuSeparator />
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem onClick={() => setMedicoSelecionado("Médico 1")}>
+                          Médico 1
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setMedicoSelecionado("Médico 2")}>
+                          Médico 2
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setMedicoSelecionado("Médico 3")}>
+                          Médico 3
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setMedicoSelecionado("Médico 4")}>
+                          Médico 4
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setMedicoSelecionado("Médico 5")}>
+                          Médico 5
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                      <DropdownMenuSeparator />
+                    </DropdownMenuContent>
+                  </DropdownMenu>
               <div className="pt-[4px] ">
-                <Button className="bg-[#7D1AD7] text-white text-base w-[116px] h-[42px] font-bold border rounded-3xl pt-[12px] pr-[32px] pb-[12px] pl-[32px]">
+                <Button className="bg-[#7D1AD7] text-white text-base w-[116px] h-[42px] font-bold border rounded-3xl pt-[12px] pr-[32px] pb-[12px] pl-[32px]"
+                  onClick={() => setMedicoSelecionado("")}>
                   Limpar
                 </Button>
+                <div className={cn("grid gap-2", className)}>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          id="date"
+                          variant={"outline"}
+                          className={cn(
+                            "w-[300px] justify-start text-left font-normal",
+                            !date && "text-muted-foreground"
+                          )}
+                        >
+                          <CalendarIcon />
+                          {date?.from ? (
+                            date.to ? (
+                              <>
+                                {format(date.from, "LLL dd, y")} -{" "}
+                                {format(date.to, "LLL dd, y")}
+                              </>
+                            ) : (
+                              format(date.from, "LLL dd, y")
+                            )
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          initialFocus
+                          mode="range"
+                          defaultMonth={date?.from}
+                          selected={date}
+                          onSelect={setDate}
+                          numberOfMonths={2}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
               </div>
             </div>
           </div>
-          <Tabs defaultValue="agendamento" className="mt-[40px] border border-red-800">
+          <Tabs defaultValue="agendamento" className="mt-[40px]">
             <TabsList className="w-[243px] h-[58px] p-2">
               <div>
                 <TabsTrigger
@@ -63,9 +156,9 @@ export default function TelaAtendimento() {
             </TabsList>
             <TabsContent
               value="agendamento"
-              className="justify-center items-center flex flex-col border border-yellow-500"
+              className="justify-center items-center flex flex-col mt-0"
             >
-              <div className="grid grid-cols-3 grid-rows-2 gap-6 border w-full border-green-500 ">
+              <div className="grid grid-cols-3 grid-rows-2 gap-6  w-full pt-8 h-[326px] overflow-scroll">
                 <CardConsultaPet
                   dataHora="18/02 13:00"
                   nomePet="Luna"
@@ -118,9 +211,9 @@ export default function TelaAtendimento() {
             </TabsContent>
             <TabsContent
               value="historico"
-              className="justify-center items-center flex flex-col"
+              className="justify-center items-center flex flex-col mt-0"
             >
-              <div className="grid grid-cols-3 grid-rows-2 gap-6 ">
+              <div className=" grid grid-cols-3 grid-rows-2 gap-6  w-full pt-8 h-[326px] overflow-scroll ">
                 <CardConsultaPet
                   dataHora="18/02 13:00"
                   nomePet="Luna"
@@ -172,7 +265,7 @@ export default function TelaAtendimento() {
               </div>
             </TabsContent>
           </Tabs>
-          <div className="flex mb-[76px] ml-auto border border-black pt-[65px] w-[256px]">
+          <div className="flex mb-[76px] ml-auto pt-[65px] w-[256px]">
             <BotaoAcao
               texto="Nova Consulta"
               cor="bg-[#50E678]"
