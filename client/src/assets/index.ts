41212b283efcc0c753e1b7792f37c09f0305871a
karinalimaklo <karinalimaolvr@gmail.com>
2025-05-29 10:40:17 -0300

    feat/tela-cadastro: creation of the registration screen

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -3,5 +3,12 @@ import LogoCITipet from "./header/logotipo citi pet.svg";
 import Heart from "./header/favorite.svg";
 import LogotipoCITi from "./header/Logotipo CITi 1.svg";
 import Cat from "./cardpet/cat 4.svg";
+import Gato from "./tela-cadastro/gato.svg";
+import Cachorro from "./tela-cadastro/cachorro1.png";
+import Girafa from "./tela-cadastro/girafa.svg";
+import Ovelha from "./tela-cadastro/ovelha.svg";
+import Porco from "./tela-cadastro/porco.svg";
+import Cavalo from "./tela-cadastro/cavalo.svg";
 
 export { LogoCITi, Cat, LogoCITipet, Heart, LogotipoCITi };
+export { Gato, Cachorro, Girafa, Ovelha, Porco, Cavalo };
