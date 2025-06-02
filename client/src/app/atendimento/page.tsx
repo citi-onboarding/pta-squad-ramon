ec08612067baaf45e7381d3b3712dcd4acca1a4d
henriquexavier1904 <hmbx@cin.ufpe.br>
2025-06-02 02:18:08 -0300

    feat/tela-atendimento: filters without integration with back end involved.

--- client/src/app/atendimento/page.tsx
+++ client/src/app/atendimento/page.tsx
@@ -11,20 +11,14 @@ import {
 import * as React from "react";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Bot, Calendar as CalendarIcon } from "lucide-react";
+import { Calendar as CalendarIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { BotaoAcao } from "@/components/Buttons";
@@ -35,10 +29,79 @@ import { useState } from "react";
 
 export default function TelaAtendimento() {
   const [medicoSelecionado, setMedicoSelecionado] = useState("");
-
   const [date, setDate] = useState<DateRange>({
     from: addDays(new Date(), 0),
     to: undefined,
+  });
+
+  const [cards] = useState([
+    {
+      dataHora: "18/02 13:00", //serve para comparação visual
+      nomePet: "Luna",
+      nomeTutor: "João Alves",
+      nomeVeterinario: "Dr. Carlos Eduardo Silva",
+      tipoConsulta: "primeira consulta",
+      imagemPet: "/assets/gato.png",
+      data: new Date(2024, 2, 18, 13, 0), //serve para filtros
+    },
+    {
+      dataHora: "18/02 13:00",
+      nomePet: "Luna",
+      nomeTutor: "João Alves",
+      nomeVeterinario: "Dra. Fernanda Costa",
+      tipoConsulta: "retorno",
+      imagemPet: "/assets/gato.png",
+      data: new Date(2024, 1, 18, 13, 0),
+    },
+    {
+      dataHora: "18/02 13:00",
+      nomePet: "Luna",
+      nomeTutor: "João Alves",
+      nomeVeterinario: "Dr. Carlos Eduardo Silva",
+      tipoConsulta: "check-up",
+      imagemPet: "/assets/cardpet/cat 4.svg",
+      data: new Date(2024, 1, 18, 13, 0),
+    },
+    {
+      dataHora: "18/02 13:00",
+      nomePet: "Luna",
+      nomeTutor: "João Alves",
+      nomeVeterinario: "Dr. José Carlos",
+      tipoConsulta: "vacinação",
+      imagemPet: "/assets/gato.png",
+      data: new Date(2024, 1, 18, 13, 0),
+    },
+    {
+      dataHora: "18/02 13:00",
+      nomePet: "Luna",
+      nomeTutor: "João Alves",
+      nomeVeterinario: "Dr. José Carlos",
+      tipoConsulta: "vacinação",
+      imagemPet: "/assets/gato.png",
+      data: new Date(2024, 1, 18, 13, 0),
+    },
+    {
+      dataHora: "18/02 13:00",
+      nomePet: "Luna",
+      nomeTutor: "João Alves",
+      nomeVeterinario: "Dr. José Carlos",
+      tipoConsulta: "primeira consulta",
+      imagemPet: "/assets/gato.png",
+      data: new Date(2024, 1, 18, 13, 0),
+    },
+  ]);
+
+  const cardsFiltrados = cards.filter((card) => {
+    const filtroMedico = medicoSelecionado
+      ? card.nomeVeterinario === medicoSelecionado
+      : true;
+
+    const filtroData =
+      date.from && date.to
+        ? card.data >= date.from && card.data <= date.to
+        : true;
+
+    return filtroMedico && filtroData;
   });
 
   return (
@@ -102,7 +165,7 @@ export default function TelaAtendimento() {
                   <DropdownMenuSeparator />
                 </DropdownMenuContent>
               </DropdownMenu>
-              <div className="pt-[4px]  flex flex-row w-full">
+              <div className="pt-[4px] flex flex-row w-full">
                 <Button
                   className="bg-[#7D1AD7] text-white text-base w-[116px] h-[42px] font-bold border rounded-3xl pt-[12px] pr-[32px] pb-[12px] pl-[32px]"
                   onClick={() => setMedicoSelecionado("")}
@@ -134,97 +197,58 @@ export default function TelaAtendimento() {
                   </div>
                 </TabsList>
                 <div className="flex justify-end items-end h-[58px] ml-auto">
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      id="date"
-                      variant={"outline"}
-                      className={cn(
-                        "w-[300px]  justify-start text-left font-normal h-[58px]",
-                        !date && "text-muted-foreground"
-                      )}
-                    >
-                      <CalendarIcon />
-                      {date?.from ? (
-                        date.to ? (
-                          <>
-                            {format(date.from, "LLL dd, y")} -{" "}
-                            {format(date.to, "LLL dd, y")}
-                          </>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        id="date"
+                        variant={"outline"}
+                        className={cn(
+                          "w-[300px]  justify-start text-left font-normal h-[58px]",
+                          !date && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon />
+                        {date?.from ? (
+                          date.to ? (
+                            <>
+                              {format(date.from, "LLL dd, y")} -{" "}
+                              {format(date.to, "LLL dd, y")}
+                            </>
+                          ) : (
+                            format(date.from, "LLL dd, y")
+                          )
                         ) : (
-                          format(date.from, "LLL dd, y")
-                        )
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      initialFocus
-                      mode="range"
-                      defaultMonth={date?.from}
-                      selected={date}
-                      onSelect={setDate}
-                      numberOfMonths={2}
-                    />
-                  </PopoverContent>
-                </Popover>
+                          <span>Pick a date</span>
+                        )}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        initialFocus
+                        mode="range" //de - até
+                        defaultMonth={date?.from}
+                        selected={date}
+                        onSelect={(range) => {
+                          if (range) setDate(range);
+                        }}
+                        numberOfMonths={2}
+                      />
+                    </PopoverContent>
+                  </Popover>
                 </div>
               </div>
               <TabsContent
                 value="agendamento"
                 className="justify-center items-center flex flex-col mt-0"
               >
-                <div className="grid grid-cols-3 gap-6  w-full pt-8 max-h-[326px] overflow-y-scroll scrollbar-none">
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="primeira consulta"
-                    imagemPet="/assets/gato.png"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="retorno"
-                    imagemPet="/assets/gato.png"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="check-up"
-                    imagemPet="././assets/cardpet/cat 4.svg"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="vacinação"
-                    imagemPet="/assets/gato.png"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="vacinação"
-                    imagemPet="/assets/gato.png"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="primeira consulta"
-                    imagemPet="/assets/gato.png"
-                  />
+                <div className="grid grid-cols-3 gap-6 w-full pt-8 max-h-[326px] overflow-y-scroll scrollbar-none">
+                  {cardsFiltrados.map((card, idx) => ( //renderização dos cards por filtro
+                    <CardConsultaPet
+                      key={idx}
+                      {...card}
+                      tipoConsulta={card.tipoConsulta as "primeira consulta" | "retorno" | "check-up" | "vacinação"}
+                    />
+                  ))}
                 </div>
               </TabsContent>
 
@@ -232,55 +256,14 @@ export default function TelaAtendimento() {
                 value="historico"
                 className="justify-center items-center flex flex-col mt-0"
               >
-                <div className=" grid grid-cols-3 gap-6  w-full pt-8 max-h-[326px] overflow-y-scroll hide-scrollbar">
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="primeira consulta"
-                    imagemPet="../assets/cardpet/cat.svg"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="retorno"
-                    imagemPet="/assets/gato.png"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="retorno"
-                    imagemPet="/assets/gato.png"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="check-up"
-                    imagemPet="/assets/gato.png"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="vacinação"
-                    imagemPet="/assets/gato.png"
-                  />
-                  <CardConsultaPet
-                    dataHora="18/02 13:00"
-                    nomePet="Luna"
-                    nomeTutor="João Alves"
-                    nomeVeterinario="Dr. José Carlos"
-                    tipoConsulta="vacinação"
-                    imagemPet="/assets/gato.png"
-                  />
+                <div className="grid grid-cols-3 gap-6 w-full pt-8 max-h-[326px] overflow-y-scroll scrollbar-none">
+                  {cardsFiltrados.map((card, idx) => (
+                    <CardConsultaPet
+                      key={idx}
+                      {...card}
+                      tipoConsulta={card.tipoConsulta as "primeira consulta" | "retorno" | "check-up" | "vacinação"}
+                    />
+                  ))}
                 </div>
               </TabsContent>
             </Tabs>
