223d5ef6d6436109f8508824d50e2429f1ab6a32
romero003 <153675281+romero003@users.noreply.github.com>
2025-06-02 20:03:40 -0300

    feat(detail-screen): added appointment history component on appointment details page

--- client/src/app/AppointmentDetails/page.tsx
+++ client/src/app/AppointmentDetails/page.tsx
@@ -4,28 +4,31 @@ import { CardHistory } from '@/components/CardHistory';
 
 export default function AppointmentsDetails() {
   return (
-<div className="h-screen">
-  <Header />
+    <div className="h-screen">
+      <Header />
 
-  <div className="flex flex-row mt-12 mx-[10%] gap-8">
-    
-    
-    <div className="w-1/2 flex flex-col">
-      <h1 className="text-5xl text-black font-source-code font-bold mb-4">
-        Atendimento
-      </h1>
-      <p className="text-gray-700">
-        Aqui você pode colocar informações, instruções ou descrição do atendimento.
-      </p>
-    </div>
+      <div className="flex flex-row mt-12 mx-[10%] gap-8">
+        <div className="w-1/2 flex flex-col">
+          <h1 className="text-5xl text-black font-source-code font-bold mb-4">
+            Atendimento
+          </h1>
+          
+        </div>
 
-    
-    <div className="w-1/2 flex flex-col ">
-    <div className="border border-gray-400 p-6 rounded-md shadow-sm">
-  <CardHistory />
-  
+        <div className="w-1/2 flex flex-col content-start">
+          <div className="flex flex-col justify-center transform scale-75">
+            <h1 className="text-lg font-bold mb-5 text-[24px]">
+              Histórico de consultas
+            </h1>
+            <div className="border border-gray-400 p-4 rounded-md  space-y-4  ">
+              <CardHistory />
+              <CardHistory />
+              <CardHistory />
+              <CardHistory />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-      
-    </div>
-  </div>
-</div>)}
+  );
+}
